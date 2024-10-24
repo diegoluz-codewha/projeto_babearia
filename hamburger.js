7794be5c6d93149af8ee8d195421aa22588dbbb5
diegoluz-codewha <diegoluz2016@hotmail.com>
2024-10-24 00:26:24 -0300

    Construção de um carrossel totalmente responsivo, combinando HTML para a estrutura, CSS para o design visual e JavaScript para adicionar interatividade

--- hamburger.js
+++ hamburger.js
@@ -3,3 +3,11 @@ document.getElementById('hamburger').addEventListener('click', function() {
     const navbarMenu = document.getElementById('navbar-menu');
     navbarMenu.classList.toggle('active');
 });
+
+let currentIndex = 1;
+const totalSlides = 3;
+
+setInterval(() => {
+    document.getElementById(`slide${currentIndex}`).checked = true;
+    currentIndex = currentIndex < totalSlides ? currentIndex + 1 : 1;
+}, 5000); // Troca a cada 5 segundos
